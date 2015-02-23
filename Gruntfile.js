2ed922c5d1bff0ac46ee1092c5baf04321d0d78a
Emil Löfquist <lofquist@gmail.com>
2015-02-23 16:03:33 +0100

    Grunt file config and basic structure

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+var sassify = require('sassify');
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      options: {
+        transform: [require('grunt-react').browserify],
+        debug: true
+      },
+      dev: {
+        src: './src/app.js',
+        dest: './tmp/app.js'
+      }
+    },
+    watch: {
+      app: {
+        files: ['src/**/.js', 'src/**/*.scss'],
+        tasks: ['browserify.dev']
+      },
+      options: {
+        livereload: true,
+        files: [
+          'src/app.js'
+        ]
+      }
+    },
+    connect: {
+      options: {
+        port: 8000,
+        hostname: 'localhost',
+        livereload: true
+      },
+      livereload: {
+        options: {
+          livereload: true,
+          base: {
+            path: 'tmp'
+          }
+        }
+      }
+    },
+    open: {
+      server: {
+        url: 'http://<%= connect.options.hostname %>:<%= connect.options.port %>'
+      }
+    }
+  });
+
+  grunt.registerTask('serve', function(target) {
+    grunt.task.run([
+      'connect:livereload',
+      'browserify:dev',
+      'open',
+      'watch:app'
+    ]);
+  });
+  grunt.registerTask('default', ['serve']);
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+
+};
