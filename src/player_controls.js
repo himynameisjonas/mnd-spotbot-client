f78e4dfb8ffb3c5ed86389b0c7027022123fceee
Emil Löfquist <lofquist@gmail.com>
2015-03-01 16:47:32 +0100

    Use react-bootstrap button

--- src/player_controls.js
+++ src/player_controls.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PlayerActions from './actions/player_actions';
+import { Button } from 'react-bootstrap';
 
 var PlayerControls = React.createClass({
   render() {
     return (
-      <div>
-        <button type="button" className="btn btn-default" onClick={PlayerActions.play}><i className="fa fa-play"></i></button>
-        <button type="button" className="btn btn-default" onClick={PlayerActions.pause}><i className="fa fa-pause"></i></button>
-        <button type="button" className="btn btn-default" onClick={PlayerActions.next}><i className="fa fa-step-forward"></i></button>
+      <div className="btn-group" role="group" aria-label="Player controls">
+        <Button bsStyle="default" onClick={PlayerActions.play}><i className="fa fa-play"></i></Button>
+        <Button bsStyle="default" onClick={PlayerActions.pause}><i className="fa fa-pause"></i></Button>
+        <Button bsStyle="default" onClick={PlayerActions.next}><i className="fa fa-forward"></i></Button>
       </div>
     );
   }
