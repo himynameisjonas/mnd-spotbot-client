c7233e7f8013d8f01a7074a3dd410c0fc34f61f3
Alexander Rothe + Emil Löfquist <dev+alexander+emil@mynewsdesk.com>
2015-05-01 11:21:28 +0200

    Make sure we don’t push to firebase too often

--- src/volume_control.js
+++ src/volume_control.js
@@ -6,19 +6,21 @@ class VolumeControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      volume: 0
+      volume: 0,
+      updating: false
     };
     this.timeout = null;
   }
 
   handleVolumeInput(e) {
     let value = e.currentTarget.value;
-    this.setState({ volume: value });
-
+    this.setState({ volume: value, updating: true });
+    if(this.state.updating) { return; }
     clearTimeout(this.timeout);
-    this.timeout = setTimeout(() =>
-      PlayerActions.setVolume(value)
-   , 100);
+    this.timeout = setTimeout(() => {
+      PlayerActions.setVolume(this.state.volume);
+      this.setState({ updating: false });
+    }, 1000);
   }
 
   componentWillReceiveProps(nextProps) {
@@ -26,15 +28,23 @@ class VolumeControl extends React.Component {
   }
 
   render() {
+
+    let volumeProps = {
+      defaultValue: this.props.volume,
+      onClick: this.handleVolumeInput.bind(this),
+      value: this.state.volume,
+      onInput: this.handleVolumeInput.bind(this),
+      step: 2
+    };
+
     return (
       <div className="volume-control">
         <i className="fa fa-volume-down"></i>
-        <input type="range" min="0" max="100" defaultValue={this.props.volume} onClick={this.handleVolumeInput.bind(this)} value={this.state.volume} onInput={this.handleVolumeInput.bind(this)} />
+        <input type="range" min="0" max="100" {...volumeProps} />
         <i className="fa fa-volume-up"></i>
       </div>
     );
   }
-
 };
 
 export default VolumeControl;
